15eff052108fe0baab9656ec57b313c611cb8952
Rafif-fw <113953893+Rafif-fw@users.noreply.github.com>
2022-09-23 11:04:14 +0700

    Add files via upload

---
+++ Pratikum 1 23 september.py
@@ -0,0 +1,18 @@
+import math
+a=int(input("masukan nilai a ="))
+b=int(input("masukan nilai b ="))
+pemjumlahan=a+b
+selisih=abs(b-a)
+hasilkali=a*b
+sisapembagian=a%b
+pembagian=a/b
+pemangkatan=a**b
+math.log10(a)
+
+print("penjumlahan a dan b    = ", pemjumlahan)
+print("selisih dari a dan b =", selisih)
+print("hasil kali dari a dan b =", hasilkali)
+print("sisa pembagian dari a dan b =", sisapembagian)
+print("pembagian dari a dan b =", pembagian)
+print("pemangkatan dari a dan b =", pemangkatan)
+print("logaritma dari a dan b =",math.log10(a) )
